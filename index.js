76236bdc8be9b6989688b3bdb90dbcc9dff60eb3
merin-frango <merinfrango2094@gmail.com>
2025-04-27 06:08:13 +0530

    update JS

---
+++ index.js
@@ -0,0 +1,11 @@
+var sideNav=document.getElementById("sideNav")
+var menuIcon=document.getElementById("menuIcon")
+var closeNav=document.getElementById("closeNav")
+
+menuIcon.addEventListener("click",function(){
+    sideNav.style.right=0
+})
+
+closeNav.addEventListener("click",function(){
+    sideNav.style.right="-50%"
+})
