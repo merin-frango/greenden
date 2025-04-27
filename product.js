d3e8bd641025d58b4bb856b9f20276e93142c73f
merin-frango <merinfrango2094@gmail.com>
2025-04-27 06:41:43 +0530

    product search

--- product.js
+++ product.js
@@ -1,11 +1,34 @@
-var sideNav=document.getElementById("sideNav")
-var menuIcon=document.getElementById("menuIcon")
-var closeNav=document.getElementById("closeNav")
+var sideNav = document.getElementById("sideNav")
+var menuIcon = document.getElementById("menuIcon")
+var closeNav = document.getElementById("closeNav")
 
-menuIcon.addEventListener("click",function(){
-    sideNav.style.right=0
+menuIcon.addEventListener("click", function () {
+    sideNav.style.right = 0
 })
 
-closeNav.addEventListener("click",function(){
-    sideNav.style.right="-50%"
+closeNav.addEventListener("click", function () {
+    sideNav.style.right = "-50%"
 })
+
+// Search
+
+var productContainer = document.getElementById("productContainer")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function () {
+    var enteredValue = event.target.value.toUpperCase()
+
+    for (count = 0; count < productlist.length; count = count + 1) {
+        var productname = productlist[count].querySelector("h1").textContent
+
+        if (productname.toUpperCase().indexOf(enteredValue)) {
+            productlist[count].style.display = "none"
+        }
+
+        else {
+            productlist[count].style.display = "block"
+        }
+    }
+})
+
